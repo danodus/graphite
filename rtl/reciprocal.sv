3f80a97ede08da3c1aca8e21d63bb937f0e09b8e
Daniel Cliche <dancliche@gmail.com>
2022-02-22 15:50:24 -0500

    Texcoord precision improved for small triangles

--- rtl/reciprocal.sv
+++ rtl/reciprocal.sv
@@ -1,20 +1,21 @@
 `include "graphite.svh"
 
-// f(x) = 1/x
-module reciprocal(
+// f(x) = NUMERATOR/x
+module reciprocal #(
+    parameter NUMERATOR = 32'h100
+)(
     input wire logic clk,
     input wire logic [31:0] x_i,
     output     logic [31:0] z_o
 );
-    localparam NUMERATOR = 32'h0x100;
-    localparam NB_BITS_PER_SUBDIVISION = 5;
-    localparam NB_SUBDIVISIONS = 65536 / (2 ** NB_BITS_PER_SUBDIVISION);
-    localparam ADJ_BITS = 16 + NB_BITS_PER_SUBDIVISION;
+    localparam NB_SUBDIVISIONS = 2048;
+    localparam SUBDIVISION_SIZE = 16384 / NB_SUBDIVISIONS;
+    localparam NB_BITS_PER_SUBDIVISION = 3;
 
-    logic [15:0] m_lut[NB_SUBDIVISIONS];
-    logic [23:0] b_lut[NB_SUBDIVISIONS];
+    logic [23:0] m_lut[NB_SUBDIVISIONS];
+    logic [31:0] b_lut[NB_SUBDIVISIONS];
 
-    logic [31:0] m, b;
+    logic [31:0] m;
 
     initial begin
         $readmemh("reciprocal_m_lut.hex", m_lut);
@@ -22,35 +23,40 @@ module reciprocal(
     end
 
     function logic [31:0] reciprocal_value(logic [31:0] x);
-        reciprocal_value = x > 0 ? rdiv(NUMERATOR << 16, x) : 32'd1 << 16;
+        reciprocal_value = x > 0 ? rdiv(NUMERATOR << 16, x) : NUMERATOR << 16;
     endfunction
 
     function logic [31:0] interpolated(logic [31:0] x);
-        interpolated = rmul(x - {x[31:ADJ_BITS], {(ADJ_BITS){1'b0}}}, {16'hFFFF, m_lut[x[31:ADJ_BITS]]}) + {8'h00, b_lut[x[31:ADJ_BITS]]};
+        if (x[31:30] > 0) begin
+            interpolated = 32'd0;
+        end else begin
+            interpolated = rmul(x - {x[31:19], {(19){1'b0}}}, {8'hFF, m_lut[x[29:19]]}) + b_lut[x[29:19]];
+        end
     endfunction
 
 /*
     initial begin
-        m_lut[0] = 16'hFFFF;
-        b_lut[0] = 24'hFFFFFF;
+        m = signed'(rdiv(NUMERATOR << 16, 32'((1) * SUBDIVISION_SIZE) << 16) - (NUMERATOR << 16)) >>> NB_BITS_PER_SUBDIVISION;
+        m_lut[0] = m[23:0];
+        b_lut[0] = NUMERATOR << 16;
+
         for (int i = 1; i < NB_SUBDIVISIONS; i++) begin
-            m = (rdiv(NUMERATOR << 16, 32'((i + 1) * 65536/NB_SUBDIVISIONS) << 16) - rdiv(NUMERATOR << 16, 32'(i * 65536/NB_SUBDIVISIONS) << 16)) >>> NB_BITS_PER_SUBDIVISION;
-            m_lut[i] = m[31:16];
-            b = rdiv(NUMERATOR << 16, 32'(i * 65536/NB_SUBDIVISIONS) << 16);
-            b_lut[i] = b[23:0];
-
-            // For some unknown reasons, the following does not work:
-            //m_lut[i] = (reciprocal_value(32'((i + 1) * 65536/NB_SUBDIVISIONS) << 16) - reciprocal_value(32'(i * 65536/NB_SUBDIVISIONS) << 16)) >>> NB_BITS_PER_SUBDIVISION;
-            //b_lut[i] = reciprocal_value(32'(i * 65536/NB_SUBDIVISIONS) << 16);
+            m = signed'(rdiv(NUMERATOR << 16, 32'((i + 1) * SUBDIVISION_SIZE) << 16) - rdiv(NUMERATOR << 16, 32'(i * SUBDIVISION_SIZE) << 16)) >>> NB_BITS_PER_SUBDIVISION;
+            m_lut[i] = m[23:0];
+            b_lut[i] = rdiv(NUMERATOR << 16, 32'(i * SUBDIVISION_SIZE) << 16);
         end
 
-        for (int i = 0; i < NB_SUBDIVISIONS; i++) begin
-            $display("%h", m_lut[i]);
-        end
+        //for (int i = 0; i < NB_SUBDIVISIONS; i++) begin
+        //    $display("%h", b_lut[i]);
+        //end
+
+        //for (int i = 0; i < 256; i++) begin
+        //    $display("%0d,%0d,%0d", i, reciprocal_value(i << 16), interpolated(i << 16));
+        //end
     end
-*/    
-
-    always_ff @(posedge clk) begin
+*/
+ 
+     always_ff @(posedge clk) begin
         //z_o = reciprocal_value(x_i);
         z_o = interpolated(x_i);
         //z_o = 32'd0;
