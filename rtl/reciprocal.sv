3100e7c7badc767d1e0be0fccbd2249a1b550ffe
Daniel Cliche <dancliche@gmail.com>
2022-03-09 14:42:29 -0500

    Removed hex files for reciprocal

--- rtl/reciprocal.sv
+++ rtl/reciprocal.sv
@@ -18,11 +18,6 @@ module reciprocal #(
 
     logic [31:0] m;
 
-    initial begin
-        $readmemh("reciprocal_m_lut.hex", m_lut);
-        $readmemh("reciprocal_b_lut.hex", b_lut);
-    end
-
     function logic [31:0] reciprocal_value(logic [31:0] x);
         reciprocal_value = x > 0 ? rdiv(NUMERATOR << 16, x) : NUMERATOR << 16;
     endfunction
@@ -35,7 +30,6 @@ module reciprocal #(
         end
     endfunction
 
-/*
     initial begin
         m = signed'(rdiv(NUMERATOR << 16, 32'((1) * SUBDIVISION_SIZE) << 16) - (NUMERATOR << 16)) >>> NB_BITS_PER_SUBDIVISION;
         m_lut[0] = m[23:0];
@@ -47,15 +41,10 @@ module reciprocal #(
             b_lut[i] = rdiv(NUMERATOR << 16, 32'(i * SUBDIVISION_SIZE) << 16);
         end
 
-        //for (int i = 0; i < NB_SUBDIVISIONS; i++) begin
-        //    $display("%h", b_lut[i]);
-        //end
-
         //for (int i = 0; i < 256; i++) begin
         //    $display("%0d,%0d,%0d", i, reciprocal_value(i << 16), interpolated(i << 16));
         //end
     end
-*/
  
     always_comb begin
         //z_o = reciprocal_value(x_i);
