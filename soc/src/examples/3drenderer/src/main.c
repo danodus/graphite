3713e2043559c17b4dc3bd090b7c79a182f7e2a5
Daniel Cliche <danclichegmail.com>
2023-11-10 10:23:45 -0500

    Add projection matrix in soc sw renderer

--- soc/src/examples/3drenderer/src/main.c
+++ soc/src/examples/3drenderer/src/main.c
@@ -8,6 +8,10 @@
 #include "matrix.h"
 #include "mesh.h"
 
+#ifndef M_PI
+#define M_PI 3.141592654
+#endif
+
 ///////////////////////////////////////////////////////////////////////////////
 // Array of triangles that sould be rendered frame by frame
 ///////////////////////////////////////////////////////////////////////////////
@@ -17,7 +21,7 @@ triangle_t* triangles_to_render = NULL;
 // Global variables for execution status and game loop
 ///////////////////////////////////////////////////////////////////////////////
 vec3_t camera_position = { 0, 0, 0 };
-float fov_factor = 640;
+mat4_t proj_matrix;
 
 bool is_running = false;
 int previous_frame_time = 0;
@@ -53,6 +57,13 @@ void setup(void) {
         window_width,
         window_height
     );
+
+    // Initialize the perspective projection matrix
+    float fov = M_PI / 3.0; // 60 deg
+    float aspect = (float)window_height / (float)window_width;
+    float znear = 0.1;
+    float zfar = 100.0;
+    proj_matrix = mat4_make_perspective(fov, aspect, znear, zfar);
 
     // Loads the cube values in the mesh data structures
     // load_cube_mesh_data();
@@ -94,17 +105,6 @@ void process_input(void) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Function that receives a 3D vector and returns a projected 2D point
-///////////////////////////////////////////////////////////////////////////////
-vec2_t project(vec3_t point) {
-    vec2_t projected_point = {
-        .x = (fov_factor * point.x) / point.z,
-        .y = (fov_factor * point.y) / point.z
-    };
-    return projected_point;
-}
-
-///////////////////////////////////////////////////////////////////////////////
 // Update function
 ///////////////////////////////////////////////////////////////////////////////
 void update(void) {
@@ -122,8 +122,8 @@ void update(void) {
 
     // Change the mesh scale/rotation per animation frame
     mesh.rotation.x += 0.1;
-    mesh.rotation.y += 0.1;
-    mesh.rotation.z += 0.1;
+    //mesh.rotation.y += 0.1;
+    //mesh.rotation.z += 0.1;
     //mesh.scale.x += 0.02;
     //mesh.scale.y += 0.01;
     //mesh.translation.x += 0.1;
@@ -195,16 +195,20 @@ void update(void) {
             }
         }
 
-        vec2_t projected_points[3];
+        vec4_t projected_points[3];
 
         // Loop all three vertices to perform projection
         for (int j = 0; j < 3; j++) {
             // Project the current vertex
-            projected_points[j] = project(vec3_from_vec4(transformed_vertices[j]));
+            projected_points[j] = mat4_mul_vec4_project(proj_matrix, transformed_vertices[j]);
 
-            // Scale and translate the projected points to the middle of the screen
-            projected_points[j].x += (window_width / 2);
-            projected_points[j].y += (window_height / 2);
+            // Scale into the view
+            projected_points[j].x *= (window_width / 2.0);
+            projected_points[j].y *= (window_height / 2.0);
+
+            // Translate the projected points to the middle of the screen
+            projected_points[j].x += (window_width / 2.0);
+            projected_points[j].y += (window_height / 2.0);
         }
 
         // Calculate the average depth of each face based on the vertices after transformations
--- soc/src/examples/3drenderer/src/matrix.c
+++ soc/src/examples/3drenderer/src/matrix.c
@@ -85,6 +85,22 @@ mat4_t mat4_make_rotation_z(float angle) {
     return m;
 }
 
+mat4_t mat4_make_perspective(float fov, float aspect, float znear, float zfar)
+{
+    // | (h/w)*1/tan(fov/2)            0           0                 0 |
+    // |                  0  1/tan(fov/2)          0                 0 |
+    // |                  0            0  zf/(zf-zn)  (-zf*zn)/(zf-zn) |
+    // |                  0            0           1                 0 |
+    mat4_t m = {{{ 0 }}};
+    m.m[0][0] = aspect * (1.0 / tan(fov / 2));
+    m.m[1][1] = 1.0 / tan(fov / 2);
+    m.m[2][2] = zfar / (zfar - znear);
+    m.m[2][3] = (-zfar * znear) / (zfar - znear);
+    m.m[3][2] = 1.0;
+
+    return m;
+}
+
 vec4_t mat4_mul_vec4(mat4_t m, vec4_t v) {
     vec4_t result;
     result.x = m.m[0][0] * v.x + m.m[0][1] * v.y + m.m[0][2] * v.z + m.m[0][3] * v.w;
@@ -102,4 +118,17 @@ mat4_t mat4_mul_mat4(mat4_t a, mat4_t b) {
         }
     }
     return m;
+}
+
+vec4_t mat4_mul_vec4_project(mat4_t mat_proj, vec4_t v) {
+    // multiply the projection matrix by our original vector
+    vec4_t result = mat4_mul_vec4(mat_proj, v);
+
+    // perform perspective divide with original z-value that is now stored in w
+    if (result.w != 0.0) {
+        result.x /= result.w;
+        result.y /= result.w;
+        result.z /= result.w;
+    }
+    return result;
 }
