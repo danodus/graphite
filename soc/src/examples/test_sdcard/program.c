377b3d2d5e7cc48634c14469e3ef2cc0193d5dca
Daniel Cliche <danclichegmail.com>
2023-11-22 11:26:10 -0500

    Add FAT16/32 support

--- soc/src/examples/test_sdcard/program.c
+++ soc/src/examples/test_sdcard/program.c
@@ -3,18 +3,88 @@
 #include <stdlib.h>
 #include <sd_card.h>
 #include <io.h>
+#include <fat_filelib.h>
+#include <fat_misc.h>
+
+sd_context_t sd_ctx;
+
+int read_sector(uint32_t sector, uint8_t *buffer, uint32_t sector_count) {
+    for (uint32_t i = 0; i < sector_count; ++i) {
+        if (!sd_read_single_block(&sd_ctx, sector + i, buffer))
+            return 0;
+        buffer += SD_BLOCK_LEN;
+    }
+    return 1;
+}
+
+int write_sector(uint32_t sector, uint8_t *buffer, uint32_t sector_count) {
+    for (uint32_t i = 0; i < sector_count; ++i) {
+        if (!sd_write_single_block(&sd_ctx, sector + i, buffer))
+            return 0;
+        buffer += SD_BLOCK_LEN;
+    }
+    return 1;
+}
+
+static void print_val(int v) {
+    char s[32];
+    uitoa(v, s, 16);
+    print(s);
+}
+
+void list_directory(const char *path)
+{
+    FL_DIR dirstat;
+
+    printf(("\r\nDirectory %s\r\n", path));
+
+    if (fl_opendir(path, &dirstat))
+    {
+        struct fs_dir_ent dirent;
+
+        while (fl_readdir(&dirstat, &dirent) == 0)
+        {
+            int d,m,y,h,mn,s;
+            fatfs_convert_from_fat_time(dirent.write_time, &h,&m,&s);
+            fatfs_convert_from_fat_date(dirent.write_date, &d,&mn,&y);
+            printf("%02d/%02d/%04d  %02d:%02d      ", d,mn,y,h,m);
+
+            if (dirent.is_dir)
+            {
+                printf("%s <DIR>\r\n", dirent.filename);
+            }
+            else
+            {
+                printf("%s [%d bytes]\r\n", dirent.filename, dirent.size);
+            }
+        }
+
+        fl_closedir(&dirstat);
+    }
+}
 
 int main(void) {
-    print("SD card test\r\n");
-
-    sd_context_t sd_ctx;
+    printf("SD card test\r\n");
 
     if (!sd_init(&sd_ctx)) {
-        print("SD card initialization failed.\r\n");
+        printf("SD card initialization failed.\r\n");
         return EXIT_FAILURE;
     }
 
-    print("SD card initialization successful.\r\n");
+    printf("SD card initialization successful.\r\n");
+
+    fl_init();
+
+    // Attach media access functions to library
+    if (fl_attach_media(read_sector, write_sector) != FAT_INIT_OK)
+    {
+        printf("Failed to init file system\r\n");
+        return EXIT_FAILURE;
+    }
+
+    list_directory("/");
+
+    printf("Success!\r\n");
 
     return EXIT_SUCCESS;
 }
